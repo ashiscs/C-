eef0a1a8156d47f48adeffa0bc1891c8c24cfce0
Ashis Kumar Singh <49238464+ashiscs@users.noreply.github.com>
2020-04-18 19:43:06 +0530

    Create floor().cpp

---
+++ floor().cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double x=1.412,y=1.891,z=1.761;
+    cout<<floor(x)<<" "<<floor(y)<<" "<<floor(z)<<"\n";
+    
+    double a=-1.412,b=-1.891,c=-1.761;
+    cout<<floor(a)<<" "<<floor(b)<<" "<<floor(c)<<"\n";
+    
+    return 0;
+}
+
+/* OUTPUT
+
+1 1 1                                                                
+-2 -2 -2 
+
+*/
